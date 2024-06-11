f5396cc4667d3d0593fa0ff050bf97421b90d1f8
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-06-11 10:52:51 +0200

    Ajoutez des fichiers projet.

---
+++ Exceptions/CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class CustomException : Exception
+    {
+
+        public DateTime Date { get; }
+        public CustomException(string message ,DateTime date)
+        {
+            Date = date;
+            base.Data.Add("Date", Date);
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int value = Int32.Parse("abc");
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException("Houston ? On a un problème !",DateTime.Now);
+            }
+        }
+
+    }
+}
